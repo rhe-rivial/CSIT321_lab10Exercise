77c14a78d01d6d26abe96c9084eee2ca3493b3c3
rhe-rivial <hisolerrhegynne@gmail.com>
2025-10-20 12:57:43 +0800

    fixed: missing @Repository

--- src/main/java/com/appdev/cruquihi/repository/UserRepository.java
+++ src/main/java/com/appdev/cruquihi/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.appdev.cruquihi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.appdev.cruquihi.entity.UserEntity;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
    
 }
